61f8ce4c09d0741532dc225542de5dbc8ae28acd
Lexona <marta.pagliuso@hotmail.com>
2025-10-25 12:29:38 +0200

    Implementata l'interfaccia grafica per la pagina del log-in

---
+++ client/src/app/pages/login/login.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+
+@Component({
+  selector: 'app-login.component',
+  imports: [],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss',
+})
+export class LoginComponent {
+  
+}
