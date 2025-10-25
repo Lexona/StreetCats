8007e74c3f4b04501d9e5fa426638b34938c85f5
Lexona <marta.pagliuso@hotmail.com>
2025-10-25 18:14:53 +0200

    Sistemata la home page per mostrare i dati dell'utente loggato e aggiunto un bottone per il logout

--- client/src/app/pages/home/home.component.ts
+++ client/src/app/pages/home/home.component.ts
@@ -1,14 +1,17 @@
 import { Component, OnInit, OnDestroy, Renderer2 } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+
+import { CommonModule } from '@angular/common';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-home',
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
 export class HomeComponent implements OnInit, OnDestroy {
-  constructor(private renderer: Renderer2) {}
+  constructor(private renderer: Renderer2, private router: Router, public authService: AuthService) {}
 
   ngOnInit(): void {
     this.renderer.addClass(document.body, 'home-background');
@@ -16,5 +19,17 @@ export class HomeComponent implements OnInit, OnDestroy {
 
   ngOnDestroy(): void {
     this.renderer.removeClass(document.body, 'home-background');
+  }
+
+  onLogout(): void {
+    this.authService.logout().subscribe({
+      next: () => {
+        console.log('Logout effettuato.');
+      },
+      error: (error)  => {
+        console.error('Errore logout: ', error);
+        this.router.navigate(['/login']);
+      }
+    });
   }
 }
